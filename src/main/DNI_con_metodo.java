b6cb2529c8795691f3d9747551426b08bdfce481
LUPUS23 <antoniodaw23@gmail.com>
2024-10-24 12:46:10 +0200

    Update

---
+++ src/main/DNI_con_metodo.java
@@ -0,0 +1,15 @@
+public class DNI_con_metodo {
+    public static char letraDNI(int dni){
+        final char [] letras = {'T', 'R', 'W', 'A', 'G', 'M', 'Y', 'F', 'P', 'D', 'X',
+                'B', 'N', 'J', 'Z', 'S', 'Q', 'V', 'H', 'L', 'C', 'K', 'E'};
+        int resto = dni % 23;
+        return letras[resto];
+    }
+    public static void main(String[] args) {
+        int dni = 54516524;
+
+        char letra = (letraDNI(dni));
+
+        System.out.println(dni + "" + letra);
+    }
+}
